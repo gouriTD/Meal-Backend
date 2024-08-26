bf21fe7923b86180182b634507f00fabdf01e07f
Gouri Tripathi <gouritripathi@Gouris-MacBook-Air.local>
2024-08-26 11:31:39 +0530

    Removing middleware check for routing

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ const app = express();
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+//   next();
+// });
 
 app.get('/',(req,res)=>{
   res.send('Connected to the server')
