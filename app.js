dab3c64ad1a8bef7ae0078e73f769d0250ca1fd4
Gouri Tripathi <gouritripathi@Gouris-MacBook-Air.local>
2024-08-27 18:42:50 +0530

    resolving CORS origin error

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 // if (corsWhitelist.indexOf(req.headers.origin) !== -1){
 // }
 
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   
