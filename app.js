4f6aac97ca478c51857f1e6b1338b4d6b1c17e74
Gouri Tripathi <gouritripathi@Gouris-MacBook-Air.local>
2024-08-27 23:44:44 +0530

    resolving url errors

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use((req, res, next) => {
 
   console.log(req.headers.origin)
   const corsWhiteList = [
+    'https://gouritd.github.io/',
     'http://localhost:5173'
   ]
   if(corsWhiteList.indexOf(req.headers.origin) !== -1){
